0a1951d8e3c6717e735a2c134803f4e002606e55
Justinscreations <jlewis9103@gmail.com>
2025-06-22 21:51:56 -0400

    lol

--- script.js
+++ script.js
@@ -88,8 +88,17 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+
+
 const darkModeToggle = document.getElementById("dark-mode-toggle");
 
 darkModeToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        darkModeToggle.textContent = "Disable Dark Mode";
+    } else {
+        darkModeToggle.textContent = "Enable Dark Mode";
+    }
 });
+
